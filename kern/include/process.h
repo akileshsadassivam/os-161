b2a5cf3fc88cc6f14b9ac057d5ac3a45890758d4
Akilesh Sadassivam <akileshsadassivam@gmail.com>
2015-02-25 22:17:06 -0500

    exit and waitpid template - before filesystem 1st merge

--- kern/include/process.h
+++ kern/include/process.h
@@ -7,6 +7,7 @@
 struct process {
 	pid_t ppid;
 	struct lock* exitlock;
+	struct cv* exitcv;
 	bool exited;
 	int exitcode;
 	struct thread* self;
@@ -17,7 +18,9 @@ struct process* process[PID_MAX];
 pid_t generate_pid(void);
 int sys_getpid(int32_t*);
 int sys_execv(userptr_t, userptr_t);
-int sys_fork(int32_t* retval, struct trapframe* tf);
+int sys_fork(int32_t*, struct trapframe*);
 void child_forkentry(void*, unsigned long);
+int sys_waitpid(int32_t*, userptr_t, userptr_t, userptr_t);
+int sys_exit(userptr_t);
 
 #endif /*_PROCESS_H*/
--- kern/syscall/process.c
+++ kern/syscall/process.c
@@ -4,11 +4,13 @@
 #include <mips/trapframe.h>
 #include <kern/errno.h>
 #include <kern/fcntl.h>
+#include <kern/wait.h>
 #include <addrspace.h>
 #include <synch.h>
 #include <current.h>
 #include <copyinout.h>
 #include <vfs.h>
+#include <syscall.h>
 #include <process.h>
 
 /* Function to generate pid for the newly created process */
@@ -43,7 +45,7 @@ sys_getpid(int32_t* retval){
 
 int
 sys_execv(userptr_t arg1, userptr_t arg2){
-	char* progname = (char*) arg1;
+	char* progname; 
 	char** args = (char**) arg2;
 
 	char** kbuffer;
@@ -52,33 +54,41 @@ sys_execv(userptr_t arg1, userptr_t arg2){
 	int result;
 	int ksize;
 	int usize;
+	int argtotalsize = 0;
 	size_t actual;
 	struct vnode *vnode;
 	vaddr_t entrypoint, stackptr;
 
-	usize = strlen(progname);
-	if(usize % 4 == 0){
-		ksize = usize;
-	}else{
-		ksize = usize + (4 - (usize % 4));
-	}
-	kbuffer[count] = kmalloc(sizeof(char) * ksize);
-
-	if((result = copyinstr((const_userptr_t)progname, kbuffer[count], ksize, &actual)) != 0){
-		return result;
+	if(strlen((char*)arg1) == 0 || args == NULL || *args == NULL){
+		return EINVAL;
 	}
 
-	for(count = 1; count < ARG_MAX; count++){
+	progname = kmalloc(strlen((char*)arg1));
+	if(progname == NULL){
+		return ENOMEM;
+	}
+
+	result = copyin(arg1, progname, strlen((char*)arg1));
+	if(result){
+		return EINVAL;
+	}
+
+	for(count = 0; count < ARG_MAX; count++){
 		if(args[count] == NULL){
 			break;
 		}else{
 			usize = strlen(args[count]);
+			if(usize == 0){
+				return EINVAL;
+			}
+
 			if(usize % 4 == 0){
 				ksize = usize;
 			} else {
 				ksize = usize + (4 - (usize % 4));
 			}
 			
+			argtotalsize += ksize;
 			kbuffer[count] = kmalloc(sizeof(char) * ksize);
 			if((result = copyinstr((const_userptr_t)args[count], kbuffer[count], ksize, &actual)) != 0){
 				return result;
@@ -87,7 +97,7 @@ sys_execv(userptr_t arg1, userptr_t arg2){
 	}
 
 	argc = count;
-	char* kargv[argc+1];			//int32_t?????
+	int32_t* kargv[argc+1];
 
 	kargv[0] = kmalloc(sizeof(int32_t));
 	*kargv[0] = (argc+1) * 4;		//'+1' -> since index starts with zero
@@ -97,10 +107,13 @@ sys_execv(userptr_t arg1, userptr_t arg2){
 		*kargv[count] = *kargv[count-1] + strlen(kbuffer[count-1]);
 	}
 
+	if(argc == 0){
+		kfree(kargv[argc]);
+	}
 	kargv[argc] = NULL;
 
 	//load the program
-	result = vfs_open(kbuffer[0], O_RDONLY, 0, &vnode);
+	result = vfs_open(progname, O_RDONLY, 0, &vnode);
 	if(result){
 		return result;
 	}
@@ -133,9 +146,34 @@ sys_execv(userptr_t arg1, userptr_t arg2){
 		return result;
 	}
 
-	
+	//Calculate the bottom of the stack based on the initial stack pointer and kernel buffer
+	vaddr_t bottomstack = stackptr - (vaddr_t)argtotalsize - (vaddr_t)((argc+1) * (sizeof(int32_t)));
+	vaddr_t curstack = bottomstack;
+	vaddr_t kargstack = bottomstack;
 
-	return 0;
+	kprintf("Bottom stack%x\n", bottomstack);
+	for(count = 0; count < argc; count++){
+		kargstack = bottomstack + (count * sizeof(int32_t));
+		curstack = bottomstack + (vaddr_t)*kargv[count];
+		kprintf("kargstack:%x\n", kargstack);
+		kprintf("curstack:%x\n", curstack);
+		//*kargv[count] = curstack;
+
+		result = copyout(&curstack, (userptr_t)kargstack, sizeof(int32_t));
+		if(result){
+			return result;
+		}
+
+		result = copyoutstr(kbuffer[count], (userptr_t)curstack, strlen(kbuffer[count]), &actual);
+		if(result){
+			return result;
+		}	
+	}
+
+	enter_new_process(argc, (userptr_t)bottomstack, bottomstack, entrypoint);
+
+	panic("enter_new_process returned");
+	return EINVAL;
 }
 
 int
@@ -190,4 +228,58 @@ child_forkentry(void* data1, unsigned long data2){
 	as_activate(curthread->t_addrspace);
 
 	mips_usermode(&stacktrapframe);
+}
+
+int
+sys_waitpid(int32_t* retval, userptr_t arg1, userptr_t arg2, userptr_t arg3){
+	pid_t pid = (pid_t)arg1;
+	int* status = (int*)arg2;
+	int options = (int)arg3;
+
+	if(status == NULL || ((int)arg2 & 16) != 0){
+		return EFAULT;
+	}
+
+	if(options != WNOHANG && options != WUNTRACED){
+		return EINVAL;
+	}
+
+	if(process[pid] == NULL){
+		return ESRCH;
+	}
+	
+	if(!process[pid]->exited){
+		if(process[pid]->ppid != curthread->t_pid){
+			return ECHILD;
+		}
+		cv_wait(process[pid]->exitcv, process[pid]->exitlock);
+	}
+	
+	*status = process[pid]->exitcode;
+	*retval = curthread->t_pid;
+
+	kfree(process[pid]->self);
+	lock_destroy(process[pid]->exitlock);
+	cv_destroy(process[pid]->exitcv);
+	kfree(process[pid]);
+
+	return 0;
+}
+
+int
+sys_exit(userptr_t exitcode){
+	pid_t pid = curthread->t_pid;
+
+	lock_acquire(process[pid]->exitlock);
+	process[pid]->exited = true;
+	lock_release(process[pid]->exitlock);
+
+	pid_t ppid = process[pid]->ppid;
+	if(!process[ppid]->exited){
+		process[pid]->exitcode = _MKWAIT_EXIT((int)exitcode);
+	}
+	thread_exit();
+
+	cv_signal(process[pid]->exitcv,process[pid]->exitlock);
+	return 0;
 }
